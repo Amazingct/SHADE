8a09aabe74a8c5e792f44c924d885f8753d4023a
Amazingct <amazingct.comptech@gmail.com>
2022-09-06 02:40:18 +0100

    web dashboard

---
+++ MainController/pages/devices.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from concurrent import futures
+from distutils.cmd import Command
+import json
+import time
+import grpc
+import ShadeShell_pb2
+import ShadeShell_pb2_grpc
+import os
+
+
+channel = grpc.insecure_channel("127.0.1.1:50054")
+ShadeShell = ShadeShell_pb2_grpc.ShadeShellStub(channel)
+devices_json = os.path.join("MainController","Configurations","devices.json")
+all_devices = {}
+
+with open(devices_json, "rb") as devices:
+    devices = json.load(devices)
+
+
+def send_command(command):
+    rx = ShadeShell.ProcessCommand(ShadeShell_pb2.command(command=command))
+    rx = json.loads(rx.response)
+    return rx["response"]
+
+
+
+for id, info in devices.items():
+    col1, col2 = st.columns(2, gap="small")
+    with st.container():
+        st.write("## {}".format(info["name"]))
+        for child in info["child"]:
+            st.write("### {}".format(child))q
+        
